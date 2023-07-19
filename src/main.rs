8c07bbb697ffddabebc6a4012fec0ce23d9df9b7
Alessandro Arcidiaco <arcidiaco.a@gmail.com>
2023-07-20 00:12:12 +0200

    initial faders settings

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,26 @@ use std::sync::{
 use std::thread;
 use std::time::Duration;
 
+struct Faders {
+    pins: HashMap<u8, u8>
+}
+
+impl Faders {
+    fn default() -> Self {
+        Self {
+            pins: HashMap::from([(18, 1), (19, 11), (20, 2), (21, 3)])
+        }
+    }
+}
 
 fn main() {
     // (arduino pin, cc)
-    let faders_pins: HashMap<u8, u8> = HashMap::from([(18, 1), (19, 11), (20, 2), (21, 3)]);
+    let mut faders: Faders = Faders::default();
+
     let mut midi = midi_term_settings().unwrap();
     let mut serial = serial_term_settings().unwrap();
-    run(&mut serial, &faders_pins, &mut midi);
+    let _ = faders_term_settings(&mut faders);
+    run(&mut serial, &faders, &mut midi);
 }
 
 fn midi_term_settings() -> Option<MidiOutputConnection> {
@@ -101,11 +114,41 @@ fn serial_term_settings() -> Option<Box<dyn SerialPort>> {
     Some(port)
 }
 
-//TODO: faders settings
+fn faders_term_settings(faders: &mut Faders) {
+    let red = Style::new().red();
+
+    println!("The actual faders CC are {}", red.apply_to("1, 11, 2, 3"));
+
+    let input: String = Input::new()
+        .with_prompt(format!(
+            "{}",
+            String::from("Press [y] to accept")
+        ))
+        .interact_text()
+        .unwrap();
+    dbg!(&input);
+    if input == "y" {
+        return;
+    } else {
+        let faders_vec: Vec<&str> = input.split(",").collect();
+        if faders_vec.len() == 4 {
+            let first_value: u8 = faders_vec[0].to_owned().parse().unwrap();
+            let second_value: u8 = faders_vec[1].to_owned().parse().unwrap();
+            let third_value: u8 = faders_vec[2].to_owned().parse().unwrap();
+            let fourth_value: u8 = faders_vec[3].to_owned().parse().unwrap();
+            *faders.pins.get_mut(&18).unwrap() = first_value;
+            *faders.pins.get_mut(&19).unwrap() = second_value;
+            *faders.pins.get_mut(&20).unwrap() = third_value;
+            *faders.pins.get_mut(&21).unwrap() = fourth_value;
+        } else {
+            println!("You have to insert at least 4 numbers");
+        }
+    }
+}
 
 fn run(
     serial: &mut Box<dyn SerialPort>,
-    faders: &HashMap<u8, u8>,
+    faders: &Faders,
     midi: &mut MidiOutputConnection,
 ) {
     let stop = Arc::new(AtomicBool::new(false));
@@ -128,7 +171,7 @@ fn run(
                 let arduino_pin = message_values[0].parse().unwrap();
                 let value = message_values[1].to_owned();
                 let parsed_value = &value[0..value.len() - 1].to_owned().parse().unwrap();
-                let cc = faders.get(&arduino_pin).unwrap();
+                let cc = faders.pins.get(&arduino_pin).unwrap();
                 let message = midi_control::control_change(Channel::Ch1, *cc, *parsed_value);
                 let message_byte: Vec<u8> = message.into();
                 let _ = &mut midi.send(&message_byte).unwrap();
@@ -153,7 +196,6 @@ fn ask_for_quitting() -> String {
             red.apply_to("Prometheus started!\n\n"),
             String::from("Press [q] to quit")
         ))
-        .with_initial_text("q")
         .interact_text()
         .unwrap();
     input
